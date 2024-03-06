c37b4ea414e8c5eed276949058be2213ba78aff6
Laia Muniesa <laia.muniesa@babelgroup.com>
2024-03-06 12:47:40 +0100

    Calculo Liquidacion

--- src/main/java/liquidaciondiversos/service/ILiquidation.java
+++ src/main/java/liquidaciondiversos/service/ILiquidation.java
@@ -1,10 +1,11 @@
 package liquidaciondiversos.service;
 
+import liquidaciondiversos.model.ProductWarranty;
+
 public interface ILiquidation {
 
     void calculoLiquidaciones();
     void comprobarExcluida();
     void calcularTipoValoracion();
-    void calcularTipoAseguramiento();
     void calcularInfraSeguro();
 }
--- src/main/java/liquidaciondiversos/service/Liquidation.java
+++ src/main/java/liquidaciondiversos/service/Liquidation.java
@@ -1,7 +1,13 @@
 package liquidaciondiversos.service;
 import liquidaciondiversos.example.BaseData;
+import liquidaciondiversos.model.ProductWarranty;
 import liquidaciondiversos.model.Sinister;
+import lombok.Data;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
 
 @Service
 public class Liquidation implements ILiquidation{
@@ -19,19 +25,77 @@ public class Liquidation implements ILiquidation{
     public void calculoLiquidaciones(){
         comprobarExcluida();
         calcularTipoValoracion();
-        calcularTipoAseguramiento();
         calcularInfraSeguro();
     }
 
     public void calcularInfraSeguro() {
     }
 
-    public void calcularTipoAseguramiento() {
+    public void calcularTipoValoracion() {
+        for (ProductWarranty warranty : sinister.getDamagedList()){
+            double importe = 0;
+            switch (warranty.getPaymentType()){
+                case PRIMER_RIESGO:
+                    importe = calcularPrimerRiesgo(warranty);
+                    break;
+                case REPOSICION_NUEVO:
+                    importe = calcularReposicionNuevo(warranty);
+                    break;
+                case VALOR_REAL:
+                    importe = calularValorReal(warranty);
+            }
+            liquidation += importe;
+        }
     }
 
-    public void calcularTipoValoracion() {
+    private double calularValorReal(ProductWarranty warranty) {
+        double valor = warranty.getCapitalInsured();
+        double depreciationAnnual = 1.0/7;
+        Data datePurchase = warranty.getDate();
+        Data dateSinister = (Data) sinister.getDate();
+
+        long age = ChronoUnit.YEARS.between((Temporal) datePurchase, (Temporal) dateSinister);
+
+        double residual = valor * 0.1;
+
+        double acutal = valor;
+        for(int i=0; i<age; i++){
+            acutal -= acutal*depreciationAnnual;
+        }
+
+        liquidation = Math.max(acutal, residual);
+        return liquidation;
+    }
+
+    private double calcularReposicionNuevo(ProductWarranty warranty) {
+        liquidation += warranty.getCapitalInsured();
+        return liquidation;
+    }
+
+    private double calcularPrimerRiesgo(ProductWarranty warranty) {
+        double valor = warranty.getCapitalInsured();
+
+        double importe = Math.max(valor, calcularToalDanos());
+        liquidation += importe;
+        return liquidation;
+    }
+
+    private double calcularToalDanos() {
+        double total = 0;
+
+        for(ProductWarranty damaged: sinister.getDamagedList()){
+            total += damaged.getCapitalInsured();
+        }
+
+        return total;
     }
 
     public void comprobarExcluida() {
+        for(ProductWarranty warranty : sinister.getDamagedList()){
+            if(warranty.isExcluded()){
+                liquidation = 0;
+                return;
+            }
+        }
     }
 }
